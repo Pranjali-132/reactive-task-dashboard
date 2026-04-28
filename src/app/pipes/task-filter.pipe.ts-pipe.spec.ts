7086c4d3cccefb121accfe17f8c89f5fd57d9dc3
Pranjali Sahu <pranjalisahu132@gmail.com>
2026-04-28 23:10:27 +0530

    removing task filter pipe

--- src/app/pipes/task-filter.pipe.ts-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TaskFilterPipeTsPipe } from './task-filter.pipe.ts-pipe';
-
-describe('TaskFilterPipeTsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TaskFilterPipeTsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
